5885d5844c9a939cf2ad9662a73549655c4d1818
pavlos.galiatsatos <pavlos.galiatsatos@fotechsolutions.com>
2018-08-16 13:23:27 +0100

    add a main.cpp file and a CMakeLists.tx file to make CLion proper parsing the dirs/files

---
+++ cmake-build-debug/CMakeFiles/sepolia3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sepolia3.dir/main.cpp.o"
+  "sepolia3.pdb"
+  "sepolia3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sepolia3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
