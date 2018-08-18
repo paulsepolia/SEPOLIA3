08cdbfdede30315a91cdace867e4b49860eadaa9
Pavlos G. Galiatsatos <paulsepolia@gmail.com>
2018-08-18 19:47:09 +0100

    remmove tmp code. add examples for DSYEVX and DSYEVR. need to finish them

--- examples/dense_diagonalizers/001_dsyev_original/0xx_clean.sh
+++ examples/dense_diagonalizers/001_dsyev_original/0xx_clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#  1.
-
    rm x_*
+   rm OUT*
+   rm out* 
---
+++ examples/dense_diagonalizers/008_dsyevr/001_compile_gnu_mkl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+  g++-8.2.0  -std=gnu++17             \
+             -Wall                    \
+             -O3                      \
+             -fopenmp                 \
+             driver_program.cpp       \
+             -L/opt/_intel/mkl/lib/intel64                                     \
+             -Wl,-R/opt/_intel/mkl/lib/intel64  -lmkl_lapack95_lp64            \
+             -lmkl_intel_lp64 -lmkl_gnu_thread -lmkl_core -lmkl_sequential -lm \
+             -o x_gnu_mkl
+
