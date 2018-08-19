159fb11141257d67e1ecb83cd6632f179169ed1b
Pavlos G. Galiatsatos <paulsepolia@gmail.com>
2018-08-19 14:02:07 +0100

    add examples for 'at' vector_dense<T> function and remove .idea from git repo

--- examples/dense_vector/run_tests.sh
+++ examples/dense_vector/run_tests.sh
@@ -4,13 +4,15 @@
 
 script_name="001_compile_gnu.sh";
 exe_name="x_gnu";
+clean_exe_name="0xx_clean.sh";
 out_name="out_all";
+
 
 for subdir in */; do
     cd "$subdir"
     ./$script_name;
     ./$exe_name >> ../out_tmp;
-    rm $exe_name
+    ./$clean_exe_name > /dev/null 2>&1 ;
     cd ..
     sort out_tmp | uniq > $out_name
     rm out_tmp
--- tests_unit/dense_vector/test_stl.cpp
+++ tests_unit/dense_vector/test_stl.cpp
@@ -83,3 +83,39 @@ TEST(vd_empty_not_preallocated, stl) {
 
     EXPECT_FALSE(vd1.empty());
 }
+
+TEST(vd_at, stl) {
+
+    vector_dense<double> vd1(dimension1, value1);
+    vector_dense<double> vd2(dimension2, value2);
+
+    EXPECT_EQ(value1, vd1);
+    EXPECT_EQ(value2, vd2);
+
+    for (uint64_t i = 0; i != vd1.size(); i++) {
+        EXPECT_EQ(value1, vd1.at(i));
+    }
+
+    for (uint64_t i = 0; i != vd2.size(); i++) {
+        EXPECT_EQ(value2, vd2.at(i));
+    }
+
+    for (auto &el: vd1) {
+        el = value2;
+    }
+
+    for (auto &el: vd2) {
+        el = value1;
+    }
+
+    EXPECT_EQ(value2, vd1);
+    EXPECT_EQ(value1, vd2);
+
+    for (uint64_t i = 0; i != vd1.size(); i++) {
+        EXPECT_EQ(value2, vd1.at(i));
+    }
+
+    for (uint64_t i = 0; i != vd2.size(); i++) {
+        EXPECT_EQ(value1, vd2.at(i));
+    }
+}
