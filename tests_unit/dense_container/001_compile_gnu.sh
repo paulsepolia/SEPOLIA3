d59bab557c968fe7276da204b4db7b9ce1dd7e3e
pavlos.galiatsatos <pavlos.galiatsatos@fotechsolutions.com>
2018-08-21 11:13:33 +0100

    improve the bash scripts to run the examples for dense_*

--- tests_unit/dense_container/001_compile_gnu.sh
+++ tests_unit/dense_container/001_compile_gnu.sh
@@ -3,18 +3,18 @@
 tests_all="constructors memory operators speed set_get algebra stl_compatibility stl"
 
 if [ $1 = 'all' ]; then
-    tests=$tests_all
+    tests=${tests_all}
 else
     tests=$1
 fi
 
-for var in $tests
+for var in ${tests}
 do
 
-    ls x_gnu_$var 2> /dev/null
+    ls x_gnu_${var} 2> /dev/null
 
     if [ $? -eq 0 ]; then
-        rm x_gnu_$var
+        rm x_gnu_${var}
     fi
 
     g++-8.2.0   -O3                             \
@@ -25,11 +25,11 @@ do
                 -isystem                        \
                 /opt/gtest/1.7.0/include        \
                 -pthread                        \
-                test_$var.cpp                   \
+                test_${var}.cpp                 \
                 driver_program.cpp              \
                 /opt/gtest/1.7.0/lib/libgtest.a \
-                -o x_gnu_$var
+                -o x_gnu_${var}
 
-    ./x_gnu_$var
+    ./x_gnu_${var}
 
 done
